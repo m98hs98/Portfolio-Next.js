03ab9c763da7d91ca2634393f767275397d316cb
Mahsa <mahsahaddadi19@gmail.com>
2024-04-13 17:46:47 +1000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
-import Services from "./components/services/Services";
+// import Services from "./components/services/Services";
 import Cursor from "./components/cursor/Cursor";
 
 
@@ -17,8 +17,8 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services"><Parallax type="services" /></section>
-      <section id=""><Services /></section>
+      {/* <section id="Services"><Parallax type="services" /></section>
+      <section id=""><Services /></section> */}
       <section id="Portfolio"><Parallax type="portfolio" /></section>
       <Portfolio />
       <section id="Contact"><Contact /></section> 
